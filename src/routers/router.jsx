b057da7843b2aa3c420da734ce2eca10bc74d188
chala16 <it22338952@my.sliit.lk>
2025-04-12 02:46:23 +0530

    add nav bar and routes

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -7,6 +7,11 @@ import AdminDashboardLayout from "../pages/restaurant_admin/AdminDashboardLayout
 import Admin_Home from "../pages/restaurant_admin/Admin_Home";
 import DeliveryDashboardLayout from "../pages/delivery_personnel/DeliveryDashboardLayout";
 import Delivery_Home from "../pages/delivery_personnel/Delivery_Home";
+import ManageItems from "../pages/restaurant_admin/ManageItems";
+import ManageOrders from "../pages/restaurant_admin/ManageOrders";
+import AddItem from "../pages/restaurant_admin/AddItem";
+import ViewItemDetails from "../pages/restaurant_admin/ViewItemDetails";
+import UpdateItem from "../pages/restaurant_admin/UpdateItem";
 
 function CreateRouter() {
   return createBrowserRouter([
@@ -34,6 +39,26 @@ function CreateRouter() {
       element: <AdminDashboardLayout />,
       children: [
         { path: "/restaurant_admin/dashboard/home", element: <Admin_Home /> },
+        { 
+          path: "/restaurant_admin/dashboard/manage-items", 
+          element: <ManageItems /> 
+        },
+        { 
+          path: "/restaurant_admin/dashboard/manage-orders", 
+          element: <ManageOrders /> 
+        },
+        { 
+          path: "/restaurant_admin/dashboard/add-item", 
+          element: <AddItem /> 
+        },
+        { 
+          path: "/restaurant_admin/dashboard/view-item/:id", 
+          element: <ViewItemDetails /> 
+        },
+        { 
+          path: "/restaurant_admin/dashboard/update-item/:id", 
+          element: <UpdateItem /> 
+        },
       ],
     },
     {
