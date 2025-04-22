d6e95b5c8fccb6444daee4aedfe362ad2aec9e61
Akila Ishan Meegoda <akilaishanmeegoda@gmail.com>
2025-04-22 10:53:51 +0530

    update nav bar and guardroutes

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -3,12 +3,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwsve0aEqNb2PMsqP-e-fIKPId8u3n87Y",
-  authDomain: "quickyshop-e41a0.firebaseapp.com",
-  projectId: "quickyshop-e41a0",
-  storageBucket: "quickyshop-e41a0.appspot.com",
-  messagingSenderId: "479381474012",
-  appId: "1:479381474012:web:8c4567e7ab6dfe2fd24219",
+  apiKey: "AIzaSyC4trkMUHbVq_F04jfeVsJnHIznNmANEtQ",
+  authDomain: "foodapp-a3b3e.firebaseapp.com",
+  projectId: "foodapp-a3b3e",
+  storageBucket: "foodapp-a3b3e.firebasestorage.app",
+  messagingSenderId: "600209739223",
+  appId: "1:600209739223:web:ad6dbce39b5602b7f74f92",
+  measurementId: "G-SH9CHMHPKC"
 };
 
 firebase.initializeApp(firebaseConfig);
