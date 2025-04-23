6c1d7e31c1aa0c2b74220e1615ed44342453f1ba
IT22074072 <it22074072@my.sliit.lk>
2025-04-23 10:44:36 +0530

    add track order page

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -29,6 +29,7 @@ import SuperAdminHome from "../pages/super_admin/SuperAdminHome.jsx";
 import Order from "../pages/customer/Order.jsx";
 import AboutUs from "../components/home/AboutUs.jsx";
 import ContactUs from "../components/home/ContactUs.jsx";
+import TrackOrder from "../components/orders/TrackOrder.jsx";
 
 function CreateRouter() {
   return createBrowserRouter([
@@ -49,6 +50,7 @@ function CreateRouter() {
         { path: "/order", element: <Order /> },
         { path: "/aboutus", element: <AboutUs /> },
         { path: "/contactus", element: <ContactUs /> },
+        {path:"/track-order/:orderId", element: <TrackOrder />}
       ],
     },
 
